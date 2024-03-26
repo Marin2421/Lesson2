a898c88897cafd7e29e4eb95414e387d4c5665cd
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-03-26 14:17:09 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,7 @@
+﻿string num = "28";
+byte n = byte.Parse(num);
+nint n1 = (int)n;
+int n2 = (int)n1;
+long n3 = (long)n2;
+double n4 = (double)n3;
+Console.WriteLine($"{n}, {n1}, {n2}, {n3}, {n4}");
